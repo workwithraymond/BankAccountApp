1c66cb3d0c66c019e6c6e428872b654196f36442
workwithraymond <workwithraymond01@gmail.com>
2024-08-18 19:53:45 -0400

    Adding to the database

--- BankAccountApp/BankAccount.cs
+++ BankAccountApp/BankAccount.cs
@@ -11,5 +11,32 @@ namespace BankAccountApp
 		public string Owner {  get; set; }
 		public Guid AccountNumber { get; set; }
 		public decimal Balance { get; set; }
+
+		public void Reg(string owner, Guid accountNumber, decimal balance)
+		{
+			SqlConnection con = new SqlConnection("Server=RDELROSARIO-22\\SQLEXPRESS02; Initial Catalog=BankAccount; Trusted_Connection=True;");
+
+			string sql = "INSERT INTO BankMember(Owner, AccountNumber, Balance) values (@Owner, @AccountNumber, @Balance)";
+
+			try
+			{
+				con.Open();
+
+				SqlCommand cmd = new SqlCommand(sql, con);
+
+				cmd.Parameters.AddWithValue("@Owner", Owner);
+				cmd.Parameters.AddWithValue("@AccountNumber", AccountNumber);
+				cmd.Parameters.AddWithValue("@Balance", Balance);
+
+				cmd.ExecuteNonQuery();
+
+			}
+			catch (SqlException ex) 
+			{
+				Console.WriteLine("SQL Error: " + ex.Message);
+			}
+
+		}
+
 	}
 }
